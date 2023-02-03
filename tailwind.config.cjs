773b69ef0a70b3028ca450159892c740516909e1
Bastien Sun <43788700+bastiensun@users.noreply.github.com>
2023-02-03 21:16:08 +0100

    chore: add eslint and prettier

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,17 @@
+// eslint-disable-next-line jsdoc/valid-types
 /** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line no-undef
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+// eslint-disable-next-line no-undef
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  theme: {
-    extend: {},
-  },
   plugins: [],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
 };
