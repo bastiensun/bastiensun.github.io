3d467159afa20474398d0eee06363a25f8d673b9
Bastien Sun <43788700+bastiensun@users.noreply.github.com>
2026-02-18 00:09:39 +0100

    test

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -7,10 +7,10 @@ export const meta: MetaFunction = () => {
 export default function Index() {
   return (
     <div className="mx-auto max-w-screen-xl p-16 flex flex-col md:flex-row items-center justify-center gap-8">
-      <img 
+      <a href="https://www.instagram.com/car_hg"><img 
         src="https://em-content.zobj.net/source/microsoft-teams/400/pig-nose_1f43d.png" 
         className="w-32 h-auto object-contain" 
-      />
+      /></a>
     
       <p className="text-left">
         Je regardrai tous<br/>
